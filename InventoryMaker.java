829e1352ec136781148de1af165fba9b878187f6
Alejandro <45500329+Alejandro12120@users.noreply.github.com>
2020-06-01 22:56:18 +0200

    Update InventoryMaker.java

--- InventoryMaker.java
+++ InventoryMaker.java
@@ -1,22 +1,32 @@
+import java.util.List;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.event.Listener;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
-public class InventoryMaker {
-	
-	public static Inventory createInventory(String title, int rows) {
-		Inventory inv = Bukkit.getServer().createInventory(null, rows*9, ChatColor.translateAlternateColorCodes('&', title));
-		return inv;
-	}
-	
-	public static ItemStack createItemStack(String name, Material material, int amount, int data) {
-		ItemStack item = new ItemStack(material, amount, (short)data);
-		ItemMeta itemmeta = item.getItemMeta();
-		itemmeta.setDisplayName(ChatColor.translateAlternateColorCodes('&', name));
-		item.setItemMeta(itemmeta);
-		return item;
-	}
-	
+public class InventoryMaker implements Listener {
+
+    public static Inventory createInventory(String title, int rows) {
+        Inventory inv = Bukkit.getServer().createInventory(null, rows * 9, Utilities.color(title));
+        return inv;
+    }
+
+    public static ItemStack createItemStack(String name, List<String> lore, Material material, int amount, int data) {
+        ItemStack item = new ItemStack(material, amount, (short) data);
+        ItemMeta itemmeta = item.getItemMeta();
+        itemmeta.setDisplayName(Utilities.color(name));
+        itemmeta.setLore(new Utilities().translateFromArray(lore));
+        item.setItemMeta(itemmeta);
+        return item;
+    }
+
+    public static ItemStack createItemStackWithoutLore(String name, Material material, int amount, int data) {
+        ItemStack item = new ItemStack(material, amount, (short) data);
+        ItemMeta itemmeta = item.getItemMeta();
+        itemmeta.setDisplayName(Utilities.color(name));
+        item.setItemMeta(itemmeta);
+        return item;
+    }
 }
